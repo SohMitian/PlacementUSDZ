d769f3155557180620b82c28eee0839e4986d6f8
soh92 <pix4soh@gmail.com>
2021-08-24 20:45:08 +0900

    Initial Commit

---
+++ PlacementUSDZ/PlacementUSDZApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlacementUSDZApp.swift
+//  PlacementUSDZ
+//
+//  Created by sho yoneda on 2021/08/24.
+//
+
+import SwiftUI
+
+@main
+struct PlacementUSDZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
